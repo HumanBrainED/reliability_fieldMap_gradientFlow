da34bdbca6fd4680715f94fa211ab1b9c00ec1a2
Jaewook Cho <jae7cho@gmail.com>
2021-09-13 09:04:06 -0400

    edit load_data()

--- code/variability_utils.py
+++ code/variability_utils.py
@@ -109,16 +109,16 @@ def icc_cmap():
     return my_cmap_r
 
 # Load ICC model output data into dict
-def load_data(tasks,path):
+def load_data(tasks,path,resultsBasename):
     data = {}
     taskname = {}
     for task in tasks:
 #         data[task] = {'raww':[],'rawb':[],'icc':[],'vartotal':[],'totmask':[],'edges':[]}
         data[task] = {'raww':[],'rawb':[],'icc':[],'vartotal':[],'totmask':[]}
-        data[task]['raww'] = pd.read_csv('%s/%s/%s_within_vector.csv' % (path,task,task)).values[:,1]
-        data[task]['rawb'] = pd.read_csv('%s/%s/%s_between_vector.csv' % (path,task,task)).values[:,1]
-        data[task]['icc'] = pd.read_csv('%s/%s/%s_icc_vector.csv' % (path,task,task)).values[:,1]
-        data[task]['vartotal'] = pd.read_csv('%s/%s/%s_vartotal_vector.csv' % (path,task,task)).values[:,1]
+        data[task]['raww'] = pd.read_csv('%s/%s/%s_within_vector.csv' % (path,task,resultsBasename)).values[:,1]
+        data[task]['rawb'] = pd.read_csv('%s/%s/%s_between_vector.csv' % (path,task,resultsBasename)).values[:,1]
+        data[task]['icc'] = pd.read_csv('%s/%s/%s_icc_vector.csv' % (path,task,resultsBasename)).values[:,1]
+        data[task]['vartotal'] = pd.read_csv('%s/%s/%s_vartotal_vector.csv' % (path,task,resultsBasename)).values[:,1]
 #         data[task]['edges'] = np.loadtxt('%s/%s/y.txt' % (path,task))
         taskname[task] = task
         between_ratio = data[task]['rawb']/data[task]['vartotal']
