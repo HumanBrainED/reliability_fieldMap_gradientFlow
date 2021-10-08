9670611a9ede1d47c1f9b763791f869b9974e691
Jaewook Cho <jae7cho@gmail.com>
2021-10-08 12:28:41 -0400

    created setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='reliability',
+      version='1.0',
+      description='Package to utilize variability field map and variability gradient flow frameworks',
+      author='Jae Wook Cho',
+      author_email='jae7cho@gmail.com',
+      url='https://github.com/jae7cho/reliability_fieldMap_gradientFlow',
+      packages=['reliability'],
+     )
