bf4e63c6dbbcd093b1b6d527e577c1e48bd95c39
Jaewook Cho <jae7cho@gmail.com>
2021-10-19 12:03:45 -0400

    Update __init__.py

--- reliability/__init__.py
+++ reliability/__init__.py
@@ -2,4 +2,4 @@ __all__ = ['gradient_flow_vectors','reliability_field_maps','variability_utils']
 
 from .gradient_flow_vectors import calc_icc_vectors,pah
 from .reliability_field_maps import plot_field_map,plot_field_map_overlay
-from .variability_utils import ICC_cmap,gradientFlow_cmap,allparcels,parcel2vert,array2mat
+from .variability_utils import ICC_cmap,gradientFlow_cmap,allparcels,parcel2vert,array2mat,lsurf,rsurf
